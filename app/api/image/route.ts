4db8c053259645c742f22e846c407d2261121257
Leroyalle <nikolajmelonov@yandex.ru>
2024-10-06 19:28:56 +0300

    try/catch image

--- app/api/image/route.ts
+++ app/api/image/route.ts
@@ -4,24 +4,27 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function GET(req: NextRequest) {
   const skip = req.nextUrl.searchParams.get('skip') || 0;
   const take = req.nextUrl.searchParams.get('take') || 12;
-  const data = await prisma.image.findMany({
-    skip: Number(skip),
-    take: Number(take),
-    where: {
-      imageItem: {
-        some: {},
+  try {
+    const data = await prisma.image.findMany({
+      skip: Number(skip),
+      take: Number(take),
+      where: {
+        imageItem: {
+          some: {},
+        },
       },
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-    include: {
-      imageItem: true,
-    },
-  });
-
-  const images = data.filter((image) => image.imageItem.length <= 2);
-  const totalCount = images.length;
-
-  return NextResponse.json({ images, totalCount });
+      orderBy: {
+        createdAt: 'desc',
+      },
+      include: {
+        imageItem: true,
+      },
+    });
+    const images = data.filter((image) => image.imageItem.length <= 2);
+    const totalCount = images.length;
+    return NextResponse.json({ images, totalCount });
+  } catch (error) {
+    console.log('Fetch Images', error);
+    return error;
+  }
 }
