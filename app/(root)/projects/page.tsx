5d99dbfc4f232eeaea9b431b6a67cb5fdc50bb1e
Leroyalle <nikolajmelonov@yandex.ru>
2024-10-06 20:23:47 +0300

    files block in projects page

--- app/(root)/projects/page.tsx
+++ app/(root)/projects/page.tsx
@@ -2,7 +2,6 @@ import { prisma } from '@/prisma/prisma-client';
 import {
   Container,
   SimilarLinks,
-  AntiCorrWrapper,
   Title,
   ProjectsWrapper,
   AnimateBlock,
@@ -11,11 +10,16 @@ import { navigationData } from '@/shared/constants';
 
 export const revalidate = 360;
 export default async function ProjectsPage() {
+  const files = await prisma.file.findMany({
+    where: {
+      category: 26,
+    },
+  });
   return (
     <Container className="px-[10px]">
       <AnimateBlock>
         <Title text={'Все проекты'} size="2xl" className={'mt-4 mb-12 pageTitle'} />
-        <ProjectsWrapper className="my-8" />
+        <ProjectsWrapper className="my-8" files={files} />
         <div className="pb-[80px] max-w-[1200px]">
           <SimilarLinks items={navigationData.bottom[0].links} />
         </div>
--- shared/components/shared/projects-wrapper/index.tsx
+++ shared/components/shared/projects-wrapper/index.tsx
@@ -5,12 +5,14 @@ import styles from './ProjectsWrapper.module.scss';
 import { navigationData } from '@/shared/constants';
 import Link from 'next/link';
 import { MoveRight, Undo2 } from 'lucide-react';
+import { FilesBlock } from '../files-block';
 
 interface Props {
+  files: File[];
   className?: string;
 }
 
-export const ProjectsWrapper: React.FC<Props> = ({ className }) => {
+export const ProjectsWrapper: React.FC<Props> = ({ files, className }) => {
   return (
     <section className={cn(styles.root, className)}>
       <ul>
@@ -23,6 +25,7 @@ export const ProjectsWrapper: React.FC<Props> = ({ className }) => {
           </li>
         ))}
       </ul>
+      <FilesBlock title={'Основные материалы:'} items={files} className="my-4" />
     </section>
   );
 };
